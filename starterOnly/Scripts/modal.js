d5e9ba9556a71cd728fad7729bde88b9e935bc8a
FZChlo <bibiefoli@hotmail.fr>
2022-01-12 14:31:53 +0100

    Close btn modal

--- starterOnly/Scripts/modal.js
+++ starterOnly/Scripts/modal.js
@@ -7,15 +7,35 @@ function editNav() {
     }
 }
 
-// DOM Elements
+// DOM ELEMENTS
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelectorAll("#close");
+
+//FORM ELEMENTS
+const firstElt = document.getElementById("first");
+const lastElt = document.getElementById("last");
+const emailElt = document.getElementById("email");
+const birthdateElt = document.getElementById("birthdate");
+const quantityElt = document.getElementById("quantity");
+const cityElt = document.querySelector("input[type=radio]");
+const conditionsElt = document.getElementById("checkbox1");
+
+//EVENTS
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// close modal event
+closeModalBtn.forEach(elt => elt.addEventListener("click", closeModal));
+
 // launch modal form
 function launchModal() {
     modalbg.style.display = "block";
+}
+
+// close modal form
+function closeModal() {
+    modalbg.style.display = "none";
 }
